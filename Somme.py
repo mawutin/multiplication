1ee888534e9ff4afc0e714872feb6c1384833d98
mawutin <leonie.dagoudo@imsp-uac.org>
2025-08-19 13:17:42 +0100

    table de multiplication

---
+++ Somme.py
@@ -0,0 +1,5 @@
+#coding:utf-8
+x=float(input('enter first nomber'))
+y=float(input('enter second nomber'))
+results=x+y
+print("the sum of both number is",results)
---
+++ table_de_multiplication.py
@@ -0,0 +1,34 @@
+nombre= int(input('entrer un nombre: '))
+
+a=nombre*1
+print(nombre,"x 1 =", a)
+
+b=nombre*2 
+print(nombre,"x 2 =", b)
+
+c=nombre*3 
+print(nombre,"x 3 =", c)
+
+d=nombre*4 
+print(nombre,"x 4 =", d)
+
+e=nombre*6 
+print(nombre,"x 6 =", e)
+
+f=nombre*7 
+print(nombre,"x 7 =", f)
+
+g=nombre*8 
+print(nombre,"x 8 =", g)
+
+h=nombre*9 
+print(nombre,"x 9 =", h)
+
+i=nombre*10 
+print(nombre,"x 10 =", i)
+
+j=nombre*11
+print(nombre,"x 11 =", j)
+
+k=nombre*12
+print(nombre,"x 12 =", k)
